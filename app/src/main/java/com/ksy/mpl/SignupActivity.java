6149c29fb28b227c2722a72f2d34c9fdb80cf6ab
김신영 <lukey0515@naver.com>
2022-11-19 16:46:29 +0900

    //connect SignUpActivity & LoginActivity to Firebase

--- app/src/main/java/com/ksy/mpl/SignupActivity.java
+++ app/src/main/java/com/ksy/mpl/SignupActivity.java
@@ -1,14 +1,84 @@
 package com.ksy.mpl;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
 public class SignupActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    Button signupBtn;
+    EditText idTextView, emailTextView, passWordTextView, passwordCheckTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        firebaseAuth =  FirebaseAuth.getInstance();
+        signupBtn = (Button) findViewById(R.id.signupBtn);
+        idTextView = (EditText) findViewById(R.id.idTextView);
+        emailTextView = (EditText) findViewById(R.id.emailTextView);
+        passWordTextView = (EditText) findViewById(R.id.passWordTextView);
+        passwordCheckTextView = (EditText) findViewById(R.id.passwordCheckTextView);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String id = idTextView.getText().toString().trim();
+                final String email = emailTextView.getText().toString().trim();
+                String password = passWordTextView.getText().toString().trim();
+                String passwordCheck = passwordCheckTextView.getText().toString().trim();
+
+                if (password.equals(passwordCheck)) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = firebaseAuth.getCurrentUser();
+                                String email = user.getEmail();
+                                String uid = user.getUid();
+                                String name = id;
+
+                                HashMap<Object, String> hashMap = new HashMap<>();
+
+                                hashMap.put("uid", uid);
+                                hashMap.put("email", email);
+                                hashMap.put("name", name);
+
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                DatabaseReference reference = database.getReference("Users");
+                                reference.child(uid).setValue(hashMap);
+
+                                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                Toast.makeText(SignupActivity.this, "이미 존재하는 아이디입니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(SignupActivity.this, "비밀번호를 다시 확인해주세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
     }
 }
